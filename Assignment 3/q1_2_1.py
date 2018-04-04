d35d3ac553153c81e76bf1ba25b7bf4d28e07fe9
Balu Nathan <balu.nathan.04@gmail.com>
2018-04-04 18:12:33 -0400

    More naming errors

--- Assignment 3/q1_2_1.py
+++ Assignment 3/q1_2_1.py
@@ -44,7 +44,7 @@ def build_layer(X, num_hidden_units):
     
     # initialize the weight matrix and bias vector
     num_inputs = X.get_shape().as_list()[-1]
-    print("num hidden units:", hidden_units)
+    print("num hidden units:", num_hidden_units)
     W = tf.get_variable(name="Weights", shape=(num_inputs, num_hidden_units), dtype=tf.float64, initializer=tf.contrib.layers.xavier_initializer())
     b = tf.Variable(tf.zeros(shape=(1, num_hidden_units), dtype=tf.float64, name="Bias"))
 
